ab3db23068765f07a078e5a0fb64166a94c33830
Daniel McKnight <daniel@neon.ai>
2024-02-02 09:41:47 -0800

    Simplify installer for debos

--- install.sh
+++ install.sh
@@ -1,131 +1,76 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root (use sudo)" 1>&2
-   exit 1
-fi
-
-# Check for enough space on /boot volume
-boot_line=$(df -h | grep /boot | head -n 1)
-if [ "x${boot_line}" = "x" ]; then
-  echo "Warning: /boot volume not found .."
-else
-  boot_space=$(echo $boot_line | awk '{print $4;}')
-  free_space=$(echo "${boot_space%?}")
-  unit="${boot_space: -1}"
-  if [[ "$unit" = "K" ]]; then
-    echo "Error: Not enough space left ($boot_space) on /boot"
-    exit 1
-  elif [[ "$unit" = "M" ]]; then
-    if [ "$free_space" -lt "25" ]; then
-      echo "Error: Not enough space left ($boot_space) on /boot"
-      exit 1
-    fi
-  fi
-fi
-
-#
-# make sure that we are on something ARM/Raspberry related
-# either a bare metal Raspberry or a qemu session with 
-# Raspberry stuff available
-# - check for /boot/overlays
-# - dtparam and dtoverlay is available
-errorFound=0
 OVERLAYS=/boot/overlays
 [ -d /boot/firmware/overlays ] && OVERLAYS=/boot/firmware/overlays
 
-if [ ! -d $OVERLAYS ] ; then
-  echo "$OVERLAYS not found or not a directory" 1>&2
-  errorFound=1
-fi
-# should we also check for alsactl and amixer used in seeed-voicecard?
-PATH=$PATH:/opt/vc/bin
-for cmd in dtparam dtoverlay ; do
-  if ! which $cmd &>/dev/null ; then
-    echo "$cmd not found" 1>&2
-    echo "You may need to run ./ubuntu-prerequisite.sh"
-    errorFound=1
-  fi
-done
-if [ $errorFound = 1 ] ; then
-  echo "Errors found, exiting." 1>&2
-  exit 1
-fi
 
 ver="0.3"
-uname_r=$(uname -r)
 
 # we create a dir with this version to ensure that 'dkms remove' won't delete
 # the sources during kernel updates
 marker="0.0.0"
 
 _VER_RUN=
-function get_kernel_version() {
-  local ZIMAGE IMG_OFFSET
+#function get_kernel_version() {
+#  local ZIMAGE IMG_OFFSET
+#
+#  _VER_RUN=""
+#  [ -z "$_VER_RUN" ] && {
+#    ZIMAGE=/boot/kernel.img
+#    [ -f /boot/firmware/vmlinuz ] && ZIMAGE=/boot/firmware/vmlinuz
+#    IMG_OFFSET=$(LC_ALL=C grep -abo $'\x1f\x8b\x08\x00' $ZIMAGE | head -n 1 | cut -d ':' -f 1)
+#    _VER_RUN=$(dd if=$ZIMAGE obs=64K ibs=4 skip=$(( IMG_OFFSET / 4)) 2>/dev/null | zcat | grep -a -m1 "Linux version" | strings | awk '{ print $3; }')
+#  }
+#  echo "$_VER_RUN"
+#  return 0
+#}
 
-  _VER_RUN=""
-  [ -z "$_VER_RUN" ] && {
-    ZIMAGE=/boot/kernel.img
-    [ -f /boot/firmware/vmlinuz ] && ZIMAGE=/boot/firmware/vmlinuz
-    IMG_OFFSET=$(LC_ALL=C grep -abo $'\x1f\x8b\x08\x00' $ZIMAGE | head -n 1 | cut -d ':' -f 1)
-    _VER_RUN=$(dd if=$ZIMAGE obs=64K ibs=4 skip=$(( IMG_OFFSET / 4)) 2>/dev/null | zcat | grep -a -m1 "Linux version" | strings | awk '{ print $3; }')
-  }
-  echo "$_VER_RUN"
-  return 0
-}
+#function check_kernel_headers() {
+#  VER_RUN=$(get_kernel_version)
+#  VER_HDR=$(dpkg -L raspberrypi-kernel-headers | egrep -m1 "/lib/modules/[^-]+/build" | awk -F'/' '{ print $4; }')
+#  [ "X$VER_RUN" == "X$VER_HDR" ] && {
+#    return 0
+#  }
+#  VER_HDR=$(dpkg -L linux-headers-$VER_RUN | egrep -m1 "/lib/modules/[[:print:]]+/build" | awk -F'/' '{ print $4; }')
+#  [ "X$VER_RUN" == "X$VER_HDR" ] && {
+#    return 0
+#  }
+#
+#  # echo RUN=$VER_RUN HDR=$VER_HDR
+#  echo " !!! Your kernel version is $VER_RUN"
+#  echo "     Not found *** corresponding *** kernel headers with apt-get."
+#  echo "     This may occur if you have ran 'rpi-update'."
+#  echo " Choose  *** y *** will revert the kernel to version $VER_HDR then continue."
+#  echo " Choose  *** N *** will exit without this driver support, by default."
+#  read -p "Would you like to proceed? (y/N)" -n 1 -r -s
+#  echo
+#  if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+#    exit 1;
+#  fi
+#
+#  apt-get -y --reinstall install raspberrypi-kernel
+#}
 
-function check_kernel_headers() {
-  VER_RUN=$(get_kernel_version)
-  VER_HDR=$(dpkg -L raspberrypi-kernel-headers | egrep -m1 "/lib/modules/[^-]+/build" | awk -F'/' '{ print $4; }')
-  [ "X$VER_RUN" == "X$VER_HDR" ] && {
-    return 0
-  }
-  VER_HDR=$(dpkg -L linux-headers-$VER_RUN | egrep -m1 "/lib/modules/[[:print:]]+/build" | awk -F'/' '{ print $4; }')
-  [ "X$VER_RUN" == "X$VER_HDR" ] && {
-    return 0
-  }
-
-  # echo RUN=$VER_RUN HDR=$VER_HDR
-  echo " !!! Your kernel version is $VER_RUN"
-  echo "     Not found *** corresponding *** kernel headers with apt-get."
-  echo "     This may occur if you have ran 'rpi-update'."
-  echo " Choose  *** y *** will revert the kernel to version $VER_HDR then continue."
-  echo " Choose  *** N *** will exit without this driver support, by default."
-  read -p "Would you like to proceed? (y/N)" -n 1 -r -s
-  echo
-  if ! [[ $REPLY =~ ^[Yy]$ ]]; then
-    exit 1;
-  fi
-
-  apt-get -y --reinstall install raspberrypi-kernel
-}
-
-# update and install required packages
-which apt &>/dev/null
-if [[ $? -eq 0 ]]; then
-  apt update -y
-  # Raspbian kernel packages
-  apt-get -y install raspberrypi-kernel-headers raspberrypi-kernel 
-  # Ubuntu kernel packages
-  apt-get -y install linux-raspi linux-headers-raspi linux-image-raspi
-  apt-get -y install dkms git i2c-tools libasound2-plugins
-  # rpi-update checker
-  check_kernel_headers
-fi
-
-# Arch Linux
-which pacman &>/dev/null
-if [[ $? -eq 0 ]]; then
-  pacman -Syu --needed git gcc automake make dkms linux-raspberrypi-headers i2c-tools
-fi
+## update and install required packages
+#which apt &>/dev/null
+#if [[ $? -eq 0 ]]; then
+#  apt update -y
+#  # Raspbian kernel packages
+#  apt-get -y install raspberrypi-kernel-headers raspberrypi-kernel
+#  # Ubuntu kernel packages
+#  apt-get -y install linux-raspi linux-headers-raspi linux-image-raspi
+#  apt-get -y install dkms git i2c-tools libasound2-plugins
+#  # rpi-update checker
+#  check_kernel_headers
+#fi
 
 # locate currently installed kernels (may be different to running kernel if
 # it's just been updated)
-base_ver=$(get_kernel_version)
-base_ver=${base_ver%%[-+]*}
+#base_ver=$(get_kernel_version)
+#base_ver=${base_ver%%[-+]*}
 #kernels="${base_ver}+ ${base_ver}-v7+ ${base_ver}-v7l+"
-kernels=$(uname -r)
-
+#kernels=$(uname -r)
+kernels=($(ls /lib/modules))
 function install_module {
   local _i
 
@@ -145,10 +90,12 @@ function install_module {
   cp -a $src/* /usr/src/$mod-$ver/
 
   dkms add -m $mod -v $ver
-  for _i in $kernels; do
-    dkms build -k $_i -m $mod -v $ver && {
-      dkms install --force -k $_i -m $mod -v $ver
-    }
+  for _i in "${kernels[@]}"; do
+    if [ -d "/lib/modules/${k}/build" ]; then
+      dkms build -k $_i -m $mod -v $ver && {
+        dkms install --force -k $_i -m $mod -v $ver
+      }
+    fi
   done
 
   mkdir -p /var/lib/dkms/$mod/$ver/$marker
@@ -210,9 +157,9 @@ git --git-dir=/etc/voicecard/.git --work-tree=/etc/voicecard/ commit  -m "origin
 cp seeed-voicecard /usr/bin/
 cp seeed-voicecard.service /lib/systemd/system/
 systemctl enable  seeed-voicecard.service 
-systemctl start   seeed-voicecard
-
-echo "------------------------------------------------------"
-echo "Please reboot your raspberry pi to apply all settings"
-echo "Enjoy!"
-echo "------------------------------------------------------"
+#systemctl start   seeed-voicecard
+#
+#echo "------------------------------------------------------"
+#echo "Please reboot your raspberry pi to apply all settings"
+#echo "Enjoy!"
+#echo "------------------------------------------------------"
